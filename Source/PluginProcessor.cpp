4dbf6b83647fef10cb729da1413cff9276cbe2bd
Colter Ulrich <cju5025@gmail.com>
2021-11-05 20:36:33 -0600

    Boilerplate Refactor

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,15 +1,6 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin processor.
-
-  ==============================================================================
-*/
-
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-//==============================================================================
 GainToolAudioProcessor::GainToolAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
      : AudioProcessor (BusesProperties()
